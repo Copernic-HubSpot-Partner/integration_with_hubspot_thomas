2b720b6afe59811b7f2abe63b702b42b5d7a3b9c
thomasfromcopernic <thomas@agence-copernic.fr>
2025-03-12 12:26:54 +0100

    Modificaiton finie

--- index.js
+++ index.js
@@ -1,71 +1,67 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
+require("dotenv").config();
 const app = express();
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "pug");
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+const HUBSPOT_BASE_URL = "https://api.hubapi.com/crm/v3/objects";
+const CUSTOM_OBJECT_ID = "2-140254049";
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+// ROUTE 1 - Homepage: Affiche la liste des objets personnalisés
+app.get("/", async (req, res) => {
+  const url = `${HUBSPOT_BASE_URL}/${CUSTOM_OBJECT_ID}?properties=nom,description,attaque`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
 
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const response = await axios.get(url, { headers });
+    const data = response.data.results;
+    console.log(data);
+    res.render("homepage", { title: "Custom Objects | HubSpot APIs", data });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erreur lors de la récupération des données");
+  }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
+// ROUTE 2 - Formulaire pour ajouter un nouvel objet personnalisé
+app.get("/update-cobj", (req, res) => {
+  res.render("updates", { title: "Update Custom Object Form | HubSpot" });
 });
-*/
 
+// ROUTE 3 - Création d'un nouvel objet personnalisé
+app.post("/update-cobj", async (req, res) => {
+  const newObject = {
+    properties: {
+      nom: req.body.nom,
+      description: req.body.description,
+      attaque: req.body.attaque,
+    },
+  };
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+  const url = `${HUBSPOT_BASE_URL}/${CUSTOM_OBJECT_ID}`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
+
+  try {
+    await axios.post(url, newObject, { headers });
+    res.redirect("/");
+  } catch (error) {
+    console.error(error.response);
+    res
+      .status(500)
+      .send("Erreur lors de la création du nouvel objet personnalisé");
+  }
+});
+
+// Serveur local
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
